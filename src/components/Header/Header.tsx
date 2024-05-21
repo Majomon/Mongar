3ee382aed10cb1da9786ce1156b43b941369ccee
Ezequiel114 <ezgarcia@frba.utn.edu.ar>
2024-05-21 14:42:19 -0300

    cambiando el logo por un svg

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Logo from "@/assets/Logo.png";
 import { MenuLinks } from "../MenuLinks/MenuLinks";
 import { ButtonGeneral } from "../ButtonContact/ButtonGeneral";
 
@@ -7,7 +6,7 @@ export const Header = () => {
   return (
     <header className="w-full h-14">
       <nav className="w-full h-full px-8 flex items-center justify-between">
-        <Image src={Logo} alt="LogoMongar" width={20} height={20} />
+        <Image src={"/svg/Logo-main.svg"} alt="LogoMongar" width={38.5} height={55} />
         <MenuLinks />
         <ButtonGeneral label={""} href={""} />
       </nav>
