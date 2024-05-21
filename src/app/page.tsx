a792380a1b7c843fb6767525be6b37edf4350039
Ezequiel114 <ezgarcia@frba.utn.edu.ar>
2024-05-21 13:14:01 -0300

    locando carpetas

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <main className="w-full min-h-screen ">
+      <section className=" border border-red-600">
+        <h1 className=" text-xl font-medium">hola</h1>
+      </section>
+    </main>
+  );
+}
