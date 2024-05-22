7d9078d8b5d915d69e6fcc202452c2ff8205f306
Ezequiel114 <ezgarcia@frba.utn.edu.ar>
2024-05-22 15:00:05 -0300

    terminado el posicionamiento de los scg del hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,33 @@ import SectionWe from "../components/ui/SectionWe/SectionWe";
 
 export default function HomePage() {
   return (
-    <main className=" w-full min-h-screen">
+    <main className=" w-full min-h-screen relative overflow-x-hidden">
+      <section className=" w-full h-[580px] ">
+        <article>
+          <Image src={"/public/svg/icon-titleMain.svg"} alt="Item del titulo principal" />
+        </article>
+        <p>
+          Lorem ipsum dolor sit amet consectetur. Ut congue semper laoreet
+          tortor nunc massa vel. Cursus et ac volutpat maecenas eu nisl sed.
+          Varius enim risus pellentesque in varius
+        </p>
+
+        <Image
+          src={"/svg/Ellipse-Hero-1.svg"}
+          className=" absolute top-[-250px] left-[-150px] -z-40 "
+          width={706}
+          height={706}
+          alt="Elipce arriba"
+        />
+        <Image
+          src={"/svg/Ellipse-hero-2.svg"}
+          className=" absolute top-[100px] left-[200px] -z-40 min-[900px]:left-[150px] min-[900px]:top-[250px]"
+          width={706}
+          height={706}
+          alt="Elipce abajo"
+        />
+      </section>
       <SectionWe />
     </main>
-  )
+  );
 }
