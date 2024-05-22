083dee5310d4b58a8384029e03f1a1fdb2d601b1
Ezequiel114 <ezgarcia@frba.utn.edu.ar>
2024-05-21 22:54:37 -0300

    cambiando npg del menu-chocolar por un svg

--- src/components/ContainTextSectionWe/ContainTextSectionWe.tsx
+++ src/components/ContainTextSectionWe/ContainTextSectionWe.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import TitleGeneral from "../TitleGeneral/TitleGeneral";
+import ButtonGeneralMain from "../ButtonGeneralMain/ButtonGeneralMain";
 
 export default function ContainTextSectionWe() {
   return (
@@ -12,10 +13,7 @@ export default function ContainTextSectionWe() {
         congue semper laoreet tortor nunc massa vel. Cursus et ac volutpat
         maecenas eu nisl sed. Varius enim risus pellentesque in varius
       </p>
-      {/* Hacer componente para Button (reutilizable)*/}
-      <button className=" w-full sm:w-fit font-semibold text-lg py-3 px-6 rounded-xl border-l-2 flex items-center justify-center border-mossGreen-400 bg-gradient-to-r from-[#1DB48D]/20 to-[#0B493E]/10 relative overflow-hidden transition-all duration-500 ease-in-out  hover:scale-105 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-mossGreen-500 before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-[#fff]">
-        Lorem Button
-      </button>
+      <ButtonGeneralMain href="#" label="Lorem Button" />
     </article>
   );
 }
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,1 +1,2 @@
 export * from "./PropTitleGeneral";
+export * from "./PropButton"
