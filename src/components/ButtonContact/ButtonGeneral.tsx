f73f77f7f7116b4e83bab9442e4b3fb096c65a97
Ezequiel114 <ezgarcia@frba.utn.edu.ar>
2024-05-21 22:00:13 -0300

    terminada la modificacion otra vez

--- src/components/ButtonContact/ButtonGeneral.tsx
+++ src/components/ButtonContact/ButtonGeneral.tsx
@@ -3,8 +3,10 @@ import Link from "next/link";
 
 export const ButtonGeneral = ({ label, href }: PropsButton) => {
   return (
-    <Link href={href} className=" w-full sm:w-fit font-semibold text-lg py-3 px-6 rounded-xl border-l-2 flex items-center justify-center border-mossGreen-400 bg-gradient-to-r from-[#1DB48D]/20 to-[#0B493E]/10 relative overflow-hidden transition-all duration-500 ease-in-out  hover:scale-105 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-mossGreen-500 before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-[#fff]">
-      { label }
-    </Link>
+    <div className="hidden md:flex w-full sm:w-fit font-semibold text-lg py-3 px-6 rounded-xl border-l-2 items-center justify-center border-mossGreen-400 bg-gradient-to-r from-[#1DB48D]/20 to-[#0B493E]/10 relative overflow-hidden transition-all duration-500 ease-in-out  hover:scale-105 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-mossGreen-500 before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-[#fff]">
+      <Link href={href}>
+        <span className="text-gray-200 ">{label}</span>
+      </Link>
+    </div>
   );
 };
--- src/components/ContainTextSectionWe/ContainTextSectionWe.tsx
+++ src/components/ContainTextSectionWe/ContainTextSectionWe.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import TitleGeneral from "../TitleGeneral/TitleGeneral";
-import { ButtonGeneral } from "../ButtonContact/ButtonGeneral";
 
 export default function ContainTextSectionWe() {
   return (
@@ -14,7 +13,9 @@ export default function ContainTextSectionWe() {
         maecenas eu nisl sed. Varius enim risus pellentesque in varius
       </p>
       {/* Hacer componente para Button (reutilizable)*/}
-      <ButtonGeneral href="#" label="Button Lorem" />
+      <button className=" w-full sm:w-fit font-semibold text-lg py-3 px-6 rounded-xl border-l-2 flex items-center justify-center border-mossGreen-400 bg-gradient-to-r from-[#1DB48D]/20 to-[#0B493E]/10 relative overflow-hidden transition-all duration-500 ease-in-out  hover:scale-105 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-mossGreen-500 before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-[#fff]">
+        Lorem Button
+      </button>
     </article>
   );
 }
