a16d063a2799721826b3f0419283f393f9c50e22
Ezequiel114 <ezgarcia@frba.utn.edu.ar>
2024-05-24 00:07:22 -0300

    colocando los svg para el background y su responsivo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,14 @@ export default function HomePage() {
       />
       <SectionHero />
       <SectionWe />
-      <section className=" w-full h-fit py-28 relative flex flex-col gap-y-14 items-center justify-center">
+      <section className=" w-full h-fit py-28 relative flex flex-col gap-y-14 items-center justify-center overflow-x-hidden">
+        <Image
+          src={"/svg/Ellipse-services.svg"}
+          className=" absolute left-0 -top-1 -z-50"
+          width={706}
+          height={706}
+          alt="Elipce arriba Servicios"
+        />
         <TitleGeneral name="Servicios" />
         <article className=" flex gap-4 flex-wrap items-center justify-center">
           {/* Cards de los diversos servicios */}
@@ -42,6 +49,20 @@ export default function HomePage() {
             />
           ))}
         </article>
+        <Image
+          src={"/svg/Ellipse-hero-2.svg"}
+          className=" absolute top-[15%] md:top-[20%] left-[50%] translate-x-[-50%] md:left-[100%] -z-50 min-w-[400px]"
+          width={706}
+          height={706}
+          alt="Elipce arriba"
+        />
+        <Image
+          src={"/svg/Ellipse-hero-2.svg"}
+          className=" absolute top-[60%] left-[50%] translate-x-[-50%] -z-50 min-w-[400px] block md:hidden"
+          width={706}
+          height={706}
+          alt="Elipce arriba"
+        />
       </section>
       <Team />
       <Contact />
