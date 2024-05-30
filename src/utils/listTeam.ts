0594f19183716e47a1454e065f9ceaab27a22c2d
Mauri <mauri.monzon91@gmail.com>
2024-05-30 11:10:19 -0300

    Perfil de eze21

--- src/utils/listTeam.ts
+++ src/utils/listTeam.ts
@@ -24,29 +24,29 @@ const listTeam: CardTeams[] = [
       ],
     },
     {
-      img: "/image/Mauri.jpeg",
+      img: "/image/Eze.jpg",
       name: "Ezequiel Garcia",
       profession: "Front-end Developer, Ux/Ui Designer, Graphic Designer",
       networks: [
         {
           icon: "svg/Facebook.svg",
           name: "facebook",
-          url: "https://www.facebook.com/mauri.monzon.568",
+          url: "https://www.facebook.com/profile.php?id=100008209728155",
         },
         {
           icon: "svg/Instagram.svg",
           name: "instagram",
-          url: "https://www.instagram.com/maurimonzon_j/",
+          url: "https://www.instagram.com/ezequiel_g789/",
         },
-        {
+/*         {
           icon: "svg/Behance.svg",
           name: "behance",
           url: "",
-        },
+        }, */
         {
           icon: "svg/Github.svg",
           name: "github",
-          url: "https://github.com/Majomon",
+          url: "https://github.com/Ezequiel144",
         },
       ],
     },
